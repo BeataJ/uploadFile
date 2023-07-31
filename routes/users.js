d3f765b9db8ebb30f48fb6967a383086f698138f
beataj <beata@jasinski.com>
2023-07-31 16:08:26 -0700

    image store multer

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const multer = require('multer')
 
-const upload = multer({});
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() +  '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storageConfig});
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -15,6 +24,11 @@ router.get('/new-user', function(req, res) {
 router.post('/profiles', upload.single('image')  , (req,res) => {
   const uploadedImagesFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImagesFile);
+  console.log(userData);
+
+  res.redirect('/');
 })
 
 module.exports = router;
