48fc4be8c4af01ea7c01a8df9f909412c0d828bf
beataj <beata@jasinski.com>
2023-07-31 12:54:56 -0700

    add multer to router

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const multer = require('multer')
 
+const upload = multer({});
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -9,5 +11,10 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+router.post('/profiles', upload.single('image')  , (req,res) => {
+  const uploadedImagesFile = req.file;
+  const userData = req.body;
+})
 
 module.exports = router;
